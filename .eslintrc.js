1474ad2ef7584e69ffe9c4fe9d3c5f1a15422ce4
Oleksandr Safarov <oleksandr.safarov@mediasapiens.degit>
2021-02-11 23:28:07 +0200

    Finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+      "es6": true,
+      "browser": true,
+      "node": true
+    },
+    "rules": {
+  
+    },
+    "extends": "google"
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './module'
+import './scss/index.scss'
+
+console.log('Hi')  
+
+var a = 5
+var c = 6
